7f39f22152ffe60936e31c1880469b65b251f135
pedrojair12 <pjair3761@gmail.com>
2023-05-09 23:40:51 -0300

    atividade de teste commit

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = '989c32c922ca8225ec53696091b437bf'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuario.db'
-
-db = SQLAlchemy(app)
